a5d153939ef9524fc0207f7e7e7d48c16be922be
Zhangyi <loeyae@gmail.com>
2023-03-26 12:47:51 +0800

    更新依赖

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Licensed under the Apache License, Version 2.0 (the "License"),
 # see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
 
-__author__="Zhang Yi <loeyae@gmail.com>"
-__date__ ="$2019-2-19 9:16:07$"
-
+__author__ = "Zhang Yi <loeyae@gmail.com>"
+__date__ = "$2019-2-19 9:16:07$"
 
 from setuptools import setup, find_packages
 
 setup(
-    name = "cdspider_wemedia",
-    version = "0.1",
-    description = "数据采集框架自媒体采集",
-    author = 'Zhang Yi',
-    author_email = 'loeyae@gmail.com',
-    license = "Apache License, Version 2.0",
+    name="cdspider_wemedia",
+    version="0.1.1",
+    description="数据采集框架自媒体采集",
+    author='Zhang Yi',
+    author_email='loeyae@gmail.com',
+    license="Apache License, Version 2.0",
     url="https://github.com/loeyae/lspider_wemedia.git",
-    install_requires = [
-        'cdspider>=0.1',
+    install_requires=[
+        'cdspider>=0.1.4',
     ],
-    packages = find_packages(),
-    entry_points = {
+    packages=find_packages(),
+    entry_points={
         'cdspider.handler': [
             'wemedia-list=cdspider.handler:WemediaListHandler',
             'wemedia-item=cdspider.handler:WemediaItemHandler',
